a490259b130881247b3fa00859eb7c14cc110099
Enoumy <enoumy@gmail.com>
2022-09-24 10:35:27 -0400

    z

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
-const startElement = document.getElementById("start");
-const stopElement = document.getElementById("stop");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
 
 const displayMediaOptions = {
-    audio: false
-}
+  audio: false,
+};
 
-startElement.addEventListener("click", (_) => {
-        console.log("starting capture");
-        startCapture();
-    }, false);
+startButton.addEventListener(
+  "click",
+  startCapture,
+  false
+);
 
-
-stopElement.addEventListener("click", (_) => {
-        console.log("stopping capture");
-        stopCapture();
-    }, false);
-
-
+stopButton.addEventListener(
+  "click",
+  stopCapture,
+  false
+);
 
 var outerRecorder = null;
 
 async function startCapture() {
-    const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-    const recorder = new MediaRecorder(stream);
+  const stream = await navigator.mediaDevices.getDisplayMedia(
+    displayMediaOptions
+  );
+  const recorder = new MediaRecorder(stream);
 
-    const chunks = [];
-    recorder.ondataavailable = e => chunks.push(e.data);
-    recorder.onstop = _ => {
-        const blob = new Blob(chunks, {type : chunks[0].type});
-        console.log(blob);
-        stream.getVideoTracks()[0].stop();
-        let filename = "output.mp4";
-        if (window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveBlob(blob, filename);
-        } else {
-            var element = window.document.createElement('a');
-            element.href = window.URL.createObjectURL(blob);
-            element.download = filename;
-            document.body.appendChild(element);
-            element.click();
-            document.body.removeChild(element);
-        }
+  const chunks = [];
+  recorder.ondataavailable = (e) => chunks.push(e.data);
+  recorder.onstop = (_) => {
+    const blob = new Blob(chunks, { type: chunks[0].type });
+    stream.getVideoTracks()[0].stop();
+    let filename = "output.mp4";
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+    } else {
+      var element = window.document.createElement("a");
+      element.href = window.URL.createObjectURL(blob);
+      element.download = filename;
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
     }
+  };
 
-    outerRecorder = recorder;
-    recorder.start();
+  outerRecorder = recorder;
+  recorder.start();
 }
-
 
 function stopCapture() {
-    outerRecorder.stop();
+  console.log("Stopping recording!")
+  outerRecorder.stop();
 }
 
-console.log("End of transmission. Don't panic!")
+console.log("End of transmission. Don't panic!");
