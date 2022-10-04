a6bd626fe559a724c612698535756c43ef4b034b
Enoumy <enoumy@gmail.com>
2022-10-03 22:01:55 -0400

    z

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-npx babel-minify main.js --out-file ./build/main.js --mangle true
+
+[ ! -d "./static" ] && mkdir static
+
+echo "Minifying main.js."
+npx babel-minify main.js --out-file ./static/main.js --mangle true
+echo "DONE"
+
+echo "Minifying style.css."
+npx css-minify -d ./ -o ./static
+mv ./static/style.min.css ./static/style.css
+echo "DONE"
+
+
+echo "Moving over html; couldn't figure out how to minify it easily + not that big anyways"
+mv ./index.html ./static/index.html
 
